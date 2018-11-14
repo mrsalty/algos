60b80c6e2412aeb01f319d9f05d99db8505f4ba6
mrsalty <matteo.cunietti@gmail.com>
2018-11-14 08:50:03 +0000

    sln + BinarySearch

---
+++ Algos/Arrays/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿namespace Algos.Arrays
+{
+    public class BinarySearch
+    {
+        public int SearchArray(int[] arr, int value)
+        {
+            int leftIndex;
+            int middleIndex;
+            int rightIndex;
+            var arrLength = arr.Length;
+
+            if (arrLength <= 0) return -1;
+
+            for (leftIndex = 0, rightIndex = arrLength, middleIndex = (leftIndex + rightIndex)/2;
+                (leftIndex <=rightIndex) && arr[middleIndex] != value;
+                middleIndex = (leftIndex + rightIndex) / 2)
+            {
+                if (arr[middleIndex] > value)
+                {
+                    rightIndex = middleIndex - 1;
+                }
+                else
+                {
+                    leftIndex = middleIndex + 1;
+                }
+            }
+
+            return (leftIndex <= rightIndex) ? middleIndex : -1;
+        }
+    }
+}
---
+++ tests/Algos.Tests.Unit/BinarySearchTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Algos.Arrays;
+using Xunit;
+
+namespace Algos.Tests.Unit
+{
+    public class BinarySearchTests
+    {
+        private readonly BinarySearch _sut;
+
+        public BinarySearchTests()
+        {
+            _sut = new BinarySearch();
+        }
+
+        [Theory]
+        [InlineData(new int[] {}, 1, -1)]
+        [InlineData(new[] { 1 }, 1, 0)]
+        [InlineData(new[] { 5, 6 }, 6, 1)]
+        [InlineData(new[] { 5, 6 }, 5, 0)]
+        [InlineData(new[] { 1, 2, 3, 5, 9, 21 }, 21, 5)]
+        [InlineData(new[] { 1, 2, 3, 5, 7, 9, 21 }, 3, 2)]
+        public void WhenBinarySearch_FindsCorrectValue(int[] arr, int value, int foundIndex)
+        {
+            var result = _sut.SearchArray(arr, value);
+
+            Assert.Equal(foundIndex, result);
+        }
+    }
+}
