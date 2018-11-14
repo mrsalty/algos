791ebcc0427abb67000f0bf657891402a0766214
mrsalty <matteo.cunietti@gmail.com>
2018-11-14 09:15:35 +0000

    quicksort

---
+++ Algos/Arrays/QuickSort.cs
@@ -0,0 +1,48 @@
+﻿namespace Algos.Arrays
+{
+    public class QuickSort
+    {
+        public void Sort(int[] arr, int leftIndex, int rightIndex)
+        {
+            int pivotValue;
+            int i;
+            int j;
+
+            for (pivotValue = arr[(leftIndex + rightIndex)/2], i = leftIndex, j = rightIndex; i <= j;)
+            {
+                while (arr[i] < pivotValue)
+                {
+                    i++;
+                }
+
+                while (arr[j] > pivotValue)
+                {
+                    j--;
+                }
+
+                if (i <= j)
+                {
+                    if (i < j)
+                    {
+                        var tmp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = tmp;
+                    }
+
+                    i++;
+                    j--;
+                }
+            }
+
+            if (leftIndex < j)
+            {
+                Sort(arr, leftIndex, j);
+            }
+
+            if (i < rightIndex)
+            {
+                Sort(arr, i , rightIndex);
+            }
+        }
+    }
+}
---
+++ tests/Algos.Tests.Unit/QuickSortTests.cs
@@ -0,0 +1,26 @@
+﻿using Algos.Arrays;
+using Xunit;
+
+namespace Algos.Tests.Unit
+{
+    public class QuickSortTests
+    {
+        private readonly QuickSort _sut;
+
+        public QuickSortTests()
+        {
+            _sut = new QuickSort();
+        }
+
+        [Theory]
+        [InlineData(new[] { 1, }, new[] { 1 })]
+        [InlineData(new[] { 50, 5, 21, 12, 1, 0 }, new[] { 0, 1, 5, 12, 21, 50 })]
+        [InlineData(new[] { 1, 5, 34, 12, 7, 2, 21 }, new[] { 1, 2, 5, 7, 12, 21, 34 })]
+        public void WhenQuickSortArray_ThenArrayIsSorted(int[] arr, int[] sorted)
+        {
+            _sut.Sort(arr, 0, arr.Length - 1);
+
+            Assert.Equal(sorted, arr);
+        }
+    }
+}
