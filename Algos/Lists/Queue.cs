ec4cb15920d8d137c349149b3d204e746e2190f3
mrsalty <matteo.cunietti@gmail.com>
2018-11-15 09:04:35 +0000

    queue

---
+++ Algos/Lists/Queue.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace Algos.Lists
+{
+
+    public class Queue<T>
+    {
+        private readonly IList<QueueItem<T>> _items;
+
+        public int Count;
+
+        public Queue()
+        {
+            _items = new List<QueueItem<T>>();
+        }
+
+
+        public void Enqueue(T value)
+        {
+            var newItem = new QueueItem<T>(value);
+
+            if (Count > 0)
+            {
+                newItem.SetNext(_items[0]);
+            }
+
+            _items.Add(newItem);
+            Count++;
+        }
+
+        public QueueItem<T> Dequeue()
+        {
+            if (Count > 0)
+            {
+                var dequeued = _items[0];
+                _items.RemoveAt(0);
+                return dequeued;
+            }
+            return null;
+        }
+    }
+
+    public class QueueItem<T>
+    {
+        public QueueItem(T value)
+        {
+            Value = value;
+        }
+
+        public QueueItem(T value, QueueItem<T> next)
+        {
+            Value = value;
+            Next = next;
+        }
+
+        public QueueItem<T> Next { get; private set; }
+
+        public T Value { get; private set; }
+
+        public void SetNext(QueueItem<T> next)
+        {
+            Next = next;
+        }
+    }
+}
---
+++ tests/Algos.Tests.Unit/QueueTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Algos.Lists;
+using Xunit;
+
+namespace Algos.Tests.Unit
+{
+    public class QueueTests
+    {
+        private readonly Lists.Queue<int> _sut;
+
+        public QueueTests()
+        {
+            _sut = new Lists.Queue<int>();
+        }
+
+        [Theory]
+        [InlineData(new[] { 1 }, 1, new[] { 1 })]
+        [InlineData(new[] { 1, 3 }, 2, new[] { 1, 3 })]
+        [InlineData(new[] { 1, 3, 4, 6 }, 3, new[] { 1, 3, 4 })]
+        public void WhenEnqueueItems_ThenShouldDequeueItemsInRightOrder(int[] items, int dequeueMany, int[] expectedToBeDequeued)
+        {
+            foreach (var item in items)
+            {
+                _sut.Enqueue(item);
+            }
+
+            var dequeued = new List<int>();
+
+            for (var i = 0; i < dequeueMany; i++)
+            {
+                dequeued.Add(_sut.Dequeue().Value);
+            }
+
+            Assert.Equal(expectedToBeDequeued, dequeued);
+        }
+    }
+}
